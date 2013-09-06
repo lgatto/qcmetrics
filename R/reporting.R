c7ad91376794ba6b6ad9bf7f9343c50d1fce1aa4
Laurent <laurent.gatto@gmail.com>
2013-09-07 00:11:52 +0100

    html report with knitr

--- R/reporting.R
+++ R/reporting.R
@@ -1,59 +1,100 @@
 setMethod("qcReport", "QcMetrics",
           function(object,
                    reportname = "qcreport",
-                   type = c("knitr", "nozzle"),
+                   type = c("pdf", "tex", "nozzle", "Rmd", "html"),
                    author = Sys.getenv("USER"),
-                   title = "Quality control report generated with \\texttt{qcmetrics}",
+                   title = "Quality control report generated with qcmetrics",
                    sessioninfo = TRUE,
                    template,
                    clean = TRUE,
                    quiet = TRUE,
                    ...) {
+              if (length(object) == 0) {
+                  message("Empty 'QcMetrics' input. No report generated")
+                  return(NULL)
+              }
               type <- match.arg(type)
-              switch(type,
-                     knitr = {
-                         if (missing(template)) 
-                             template <- system.file("templates", "knitr-template.Rnw",
-                                                     package = "qcmetrics")
-                         parent <- c('<<parent, include = FALSE>>=',
-                                     paste0('set_parent("', template , '")'),
-                                     '@')
-                         title <- paste0('\\title{', title, '}')
-                         author <- paste0('\\author{', author, '}')
-                         mktitle <- "\\maketitle"
-                         ex <- lapply(seq_len(length(qcm)),
-                                      function(i) reporting_knitr(qcm, i))
-                         ex <- append(list(mktitle, parent), ex)
-                         if (sessioninfo) {
-                             si <- c("\\clearpage",
-                                     "\\section{Session information}",
-                                     "<<session-info, cache=FALSE, results = 'asis', echo=FALSE>>=",
-                                     "toLatex(sessionInfo())",
-                                     "@")                         
-                             ex <- append(ex, list(si))
-                             out <- knit(text = unlist(ex), output = paste0(reportname, ".tex"), quiet = quiet)
-                             tools::texi2pdf(out, quiet = quiet, clean = clean, ...)
-                             out <- paste0(reportname, ".pdf")
-                             moved <- file.rename(paste0(basename(reportname), ".pdf"), out)
-                             if (clean) {
-                                 file.remove(paste0(reportname, ".tex"))
-                                 unlink("figure", recursive = TRUE)
-                             }
-                             message("Report written to ", out, ".")   
-                         }
-                     }, nozzle = {
-                         dir.create(reportname)
-                         dir.create(file.path(reportname, "figure"))
-                         nozreport <- newCustomReport(reportname)
-                         for (i in 1:length(qcm))                              
-                             nozreport <- addTo(nozreport,
-                                                reporting_nozzle(qcm, i, reportname))
-                         out <- file.path(reportname, "index")
-                         writeReport(nozreport, filename = out)
-                         out <- paste0(out, ".html")
-                     })
+              qcm <- object
+              if (type %in% c("Rmd", "html")) {
+                  ext <- "Rmd"
+                  out <- paste(reportname, ext, sep = ".")
+                  con <- file(out, "w")
+                  title <- sub("qcmetrics", "`qcmetrics`", title)
+                  title <- paste0("# ", title)
+                  author <- paste0("Author: ", author, "\n")
+                  .date <- paste0("Date: ", date(), "\n\n")
+                  writeLines(c(title, author, .date), con)
+                  for (i in 1:length(object))
+                      writeLines(reporting_knitr_rmd(object, i),
+                                 con)
+                  if (sessioninfo) {
+                      si <- c("```{r echo=FALSE}",
+                              "sessionInfo()",
+                              "```")
+                      writeLines(si, con)
+                  }
+                  close(con)
+                  if (type == "html") {
+                      out <- knit2html(out, quiet = quiet)
+                      if (clean) {
+                          unlink(paste(reportname, "Rmd", sep = "."))
+                          unlink(paste(reportname, "md", sep = "."))
+                      }
+                  }
+                  message("Report written to ", out, ".")
+              }
+              if (type %in% c("tex", "pdf")) {
+                  ext <- "tex"
+                  if (missing(template)) 
+                      template <- system.file("templates", "knitr-template.Rnw",
+                                              package = "qcmetrics")
+                  parent <- c('<<parent, include = FALSE>>=',
+                              paste0('set_parent("', template , '")'),
+                              '@')
+                  title <- sub("qcmetrics", "\\\\texttt{qcmetrics}", title)
+                  title <- paste0('\\title{', title, '}')
+                  author <- paste0('\\author{', author, '}')
+                  mktitle <- "\\maketitle"
+                  ex <- lapply(seq_len(length(object)),
+                               function(i) reporting_knitr_tex(object, i))
+                  ex <- append(list(mktitle, parent), ex)
+                  if (sessioninfo) {
+                      si <- c("\\clearpage",
+                              "\\section{Session information}",
+                              "<<session-info, cache=FALSE, results = 'asis', echo=FALSE>>=",
+                              "toLatex(sessionInfo())",
+                              "@")                         
+                      ex <- append(ex, list(si))
+                  }
+                  out <- knit(text = unlist(ex),
+                              output = paste0(reportname, ".tex"),
+                              quiet = quiet)
+                  if (type == "pdf") {
+                      ext <- "pdf"
+                      tools::texi2pdf(out, quiet = quiet, clean = clean, ...)
+                      out <- paste(reportname, ext, sep = ".")
+                  }
+                  moved <- file.rename(paste(basename(reportname), ext, sep = "."), out)
+                  if (clean) {
+                      file.remove(paste0(reportname, ".tex"))
+                      unlink("figure", recursive = TRUE)
+                  }
+                  message("Report written to ", out, ".")   
+              }
+              if (type == "nozzle") {
+                  dir.create(reportname)
+                  dir.create(file.path(reportname, "figure"))
+                  nozreport <- newCustomReport(reportname)
+                  for (i in 1:length(object))                              
+                      nozreport <- addTo(nozreport,
+                                         reporting_nozzle(object, i, reportname))
+                  out <- file.path(reportname, "index")
+                  writeReport(nozreport, filename = out)
+                  out <- paste0(out, ".html")
+              }
               invisible(out)
           })
+
 
 
 
--- R/reporting_knitr.R
+++ R/reporting_knitr.R
@@ -1,4 +1,4 @@
-reporting_knitr <- function(qcm, i) {
+reporting_knitr_tex<- function(qcm, i) {
     c(paste0("\\section{", name(qcm[[i]]), "}"),
       paste0("<<", name(qcm[[i]]), ", echo=FALSE>>="),
       paste0("show(qcm[[", i, "]])"),
@@ -9,5 +9,15 @@ reporting_knitr <- function(qcm, i) {
       "@",
       "\\end{figure}",
       "\\clearpage")
+}
+
+reporting_knitr_rmd<- function(qcm, i) {
+    c(paste0("## ", name(qcm[[i]])),
+      paste0("```{r ", name(qcm[[i]]), ", echo=FALSE}"),
+      paste0("show(qcm[[", i, "]])"),
+      "```",
+      "```{r, echo=FALSE, fig.width=5, fig.height=5, fig.align='left'}",
+      paste0("plot(qcm[[", i, "]])"),
+      "```")
 }
 
